80bb6e7c13261fe7ed80874a887e07a6f4f778a6
Nhuengzii <32nhueng@gmail.com>
2023-04-13 01:38:38 +0700

    Basic music bot is ready!

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,52 @@
 import Discord from "discord.js";
 import { DisTube } from "distube";
+import setUpDistube from "./distube-setup.js";
 
 const client = new Discord.Client({
-  intents: ["Guilds", "GuildVoiceStates", "GuildMessages"],
+  intents: ["Guilds", "GuildVoiceStates", "GuildMessages", "MessageContent"],
 });
+
+const prefix = "-";
+
+const distube = new DisTube(client, {
+  searchSongs: 5,
+  searchCooldown: 10,
+  leaveOnEmpty: false,
+  leaveOnFinish: false,
+  leaveOnStop: false,
+});
+setUpDistube(distube);
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot || !message.inGuild()) return;
+  if (!message.content.startsWith(prefix)) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift();
+
+  if (["play", "p"].includes(command)) {
+    const voiceChannel = message.member?.voice?.channel;
+    if (voiceChannel) {
+      distube.play(voiceChannel, args.join(" ")),
+      {
+        message,
+        textChannel: message.channel,
+        member: message.member,
+      };
+    } else {
+      message.channel.send("You need to be in a voice channel to play music!");
+    }
+  } else if (["leave", "l"].includes(command)) {
+    distube.voices.get(message)?.leave();
+    message.channel.send("Leaving the voice channel!");
+  }
+});
+
+distube.on("addSong", (queue, song) => {
+  queue.autoplay = true;
 });
 
 const TOKEN = process.env.TOKEN;
