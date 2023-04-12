98f66e13ea16da046ce7aecdc7e4065eb7cf641b
Nhuengzii <32nhueng@gmail.com>
2023-04-13 00:57:38 +0700

    basic client login

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import Discord from "discord.js";
+import { DisTube } from "distube";
+
+const client = new Discord.Client({
+  intents: ["Guilds", "GuildVoiceStates", "GuildMessages"],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const TOKEN = process.env.TOKEN;
+client.login(TOKEN);
